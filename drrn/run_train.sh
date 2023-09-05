b75bdd8fb8d1dc0bcfa5c2297cae564cc6f7533e
Ichida <ichida@DESKTOP-0FDT588.localdomain>
2023-09-05 18:00:36 -0300

    setup

--- drrn/run_train.sh
+++ drrn/run_train.sh
@@ -1,1 +1,1 @@
-python3 train-scienceworld.py --num_envs=8 --max_steps=10000 --task_idx=13 --simplification_str=easy --priority_fraction=0.50 --memory_size=100000 --env_step_limit=100 --eval_freq=1000 --eval_set=dev --historySavePrefix=drrn-task13-results-seed0-dev
+python3 train-scienceworld.py --num_envs=8 --max_steps=10000 --task_idx=3 --simplification_str=easy --priority_fraction=0.50 --memory_size=100000 --env_step_limit=100 --eval_freq=1000 --eval_set=test --historySavePrefix=drrn-task13-results-seed0-dev
