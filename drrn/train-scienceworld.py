25f34b61a155096e4139f352c1a15f7acf87eb19
Ichida <ichida@DESKTOP-0FDT588.localdomain>
2023-09-05 18:13:51 -0300

    save_per_episode

--- drrn/train-scienceworld.py
+++ drrn/train-scienceworld.py
@@ -231,6 +231,8 @@ def train(agent, envs, max_steps, update_freq, eval_freq, checkpoint_freq, log_f
 
             envs.reset()
 
+            agent.save("-steps" + str(stepsFunctional) + "-eps" + str(numEpisodes))
+
 
     # Save anything left in history buffers
     bufferedHistorySaverTrain.saveRunHistoriesBufferIfFull(maxPerFile=args.maxHistoriesPerFile, forceSave=True)
@@ -239,7 +241,7 @@ def train(agent, envs, max_steps, update_freq, eval_freq, checkpoint_freq, log_f
 
     print("Training complete.")
     # Final save
-    agent.save("-steps" + str(stepsFunctional) + "-eps" + str(numEpisodes))
+    #agent.save("-steps" + str(stepsFunctional) + "-eps" + str(numEpisodes))
     # Close environments
     envs.close_extras()
 
