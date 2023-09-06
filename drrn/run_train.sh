ecb002b84c0920a24c5607c89abccfd1c23a170a
Ichida <alexandre.ichida@lojasrenner.com.br>
2023-09-06 15:38:54 -0300

    trained_models

--- drrn/run_train.sh
+++ drrn/run_train.sh
@@ -1,1 +1,2 @@
-python3 train-scienceworld.py --num_envs=8 --max_steps=10000 --task_idx=3 --simplification_str=easy --priority_fraction=0.50 --memory_size=100000 --env_step_limit=100 --eval_freq=1000 --eval_set=test --historySavePrefix=drrn-task13-results-seed0-dev
+python3 train-scienceworld.py --num_envs=8 --max_steps=10000 --task_idx=3 --simplification_str=easy --priority_fraction=0.50 --memory_size=100000 --env_step_limit=100 --eval_freq=1000 --eval_set=test --historySavePrefix=drrn-task13-results-seed0-dev --checkpoint_freq=1000
+
